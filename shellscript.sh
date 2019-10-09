bbc974a07b72e55c0ae747c60b27e1062422bbf9
Syed Mohammad Qasim <syed@Syeds-MacBook-Pro.local>
2019-10-09 18:26:48 +0530

    modify shell script

--- shellscript.sh
+++ shellscript.sh
@@ -1,7 +1,3 @@
-if [ ! -d 'openApi3' ] ; then
- 	git clone https://github.com/syedmohdqasim/openApi3.git  
-else 
-	cd 'openApi3'
-	git pull https://github.com/syedmohdqasim/openApi3.git 
-fi
+echo "hi"
+M_Variable = "hello"
 
