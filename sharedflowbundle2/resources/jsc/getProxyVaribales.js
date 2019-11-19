246603d268f0898bcdb960e9d7fce37fbb2c4ea8
Syed Mohammad Qasim <syed@Syeds-MacBook-Pro.local>
2019-11-19 17:13:04 +0530

    add shared flow bundle

---
+++ sharedflowbundle2/resources/jsc/getProxyVaribales.js
@@ -0,0 +1,6 @@
+print(context.getVariable('appcode'));
+print(context.getVariable('group'));
+print(context.getVariable('requiredValues'));
+print(context.getVariable('emailDL'));
+print(context.getVariable('developer.id'));
+print(context.getVariable('developer.email'));
